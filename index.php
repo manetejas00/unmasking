ddbc5b65e12272e6790ce6161e8a39e280a616fb
manetejas00 <75880380+manetejas00@users.noreply.github.com>
2025-01-26 16:52:59 +0530

    updated

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+$url = urldecode(parse_url($_SERVER['REQUEST_URL'], PHP_URL_PATH) ?? '');
+if ($url !== '/' && file_exists(__DIR__ . '/public' . $url)) {
+    return false;
+}
+require_once __DIR__ . '/public/index.php';

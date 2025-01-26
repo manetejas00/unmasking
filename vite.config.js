9c94d5ffc53139239ddbd73b7c84a995f0a772b7
manetejas00 <75880380+manetejas00@users.noreply.github.com>
2025-01-26 16:28:19 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import crypto from 'crypto-browserify';
 
 export default defineConfig({
+    define: {
+        'global.crypto': crypto,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],

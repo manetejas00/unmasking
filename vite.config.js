983ffdc6c6d94542cfaca978bb3c6705c10a04c8
manetejas00 <75880380+manetejas00@users.noreply.github.com>
2025-01-29 11:21:57 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,21 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import crypto from 'crypto-browserify';
 
 export default defineConfig({
-    define: {
-        'global.crypto': crypto,
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
+      buffer: 'buffer',
     },
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  },
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  optimizeDeps: {
+    include: ['crypto-browserify'],
+  },
 });
